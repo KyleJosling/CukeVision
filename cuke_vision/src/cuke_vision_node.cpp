a8f9559ebabfd18be922fd0f0df78a7fb07dd27f
Kyle Josling <kylejosling@hotmail.com>
2019-11-08 17:13:52 -0500

    Neural network is trained. Started constructing real cuke_vision node.

--- cuke_vision/src/cuke_vision_node.cpp
+++ cuke_vision/src/cuke_vision_node.cpp
@@ -18,44 +18,111 @@
 
 #include "cuke_vision/detectObject.hpp"
 
+// Remove the bounding boxes with low confidence using non-maxima suppression
+void postprocess(cv::Mat& frame, const std::vector<cv::Mat>& out);
 
-class ardrone_object_tracker {
+// Get the names of the output layers
+std::vector<cv::String> getOutputsNames(const cv::dnn::Net& net);
+
+class object_detector {
 
     public:
 
         // Initialization list
-        ardrone_object_tracker():
+        object_detector():
 
         // Initialize image transport instance
         it(nH) { 
+            
+            // Load classes
+            std::string line;
+            std::ifstream ifs(classesFile.c_str());
+            while (getline(ifs, line)) classes.push_back(line);
+
+            // Load the network
+            net = cv::dnn::readNetFromDarknet(modelConfiguration, modelWeights);
+
+            // Default backend should be OpenCV
+            net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+            net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+
+            #ifdef GUI
+            namedWindow(kWinName, cv::WINDOW_NORMAL);
+            #endif
 
         }
 
         void imageCallback(const sensor_msgs::ImageConstPtr &image_msg) {
 
+            try {
+
+                frame = cv_bridge::toCvCopy(image_msg, "bgr8")->image;
+
+                // Create a 4D blob from a frame.
+                cv::dnn::blobFromImage(frame, blob, 1/255.0, cvSize(inpWidth, inpHeight), cv::Scalar(0,0,0), true, false);
+
+                //Sets the input to the network
+                net.setInput(blob);
+
+                // Runs the forward pass to get output of the output layers
+                std::vector<cv::Mat> outs;
+                // TODO is this right? old function was causing linker errors 
+                net.forward(outs, getOutputsNames(net));
+
+                // Remove the bounding boxes with low confidence
+                postprocess(frame, outs);
+
+                // Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+                std::vector<double> layersTimes;
+                double freq = cv::getTickFrequency() / 1000;
+                double t = net.getPerfProfile(layersTimes) / freq;
+                std::string label = cv::format("Inference time for a frame : %.2f ms", t);
+
+                #ifdef GUI
+                cv::putText(frame, label, cv::Point(0, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255));
+                cv::imshow(kWinName, frame);
+                #endif
+
+            } catch (cv_bridge::Exception &e) {
+                ROS_ERROR("Image encoding error %s", e.what());
+            }
         }
         
     private:
-        const std::string arImageTopic = "/ardrone/front/image_raw";
-        const std::string objectTopic = "/object";
+
+        const std::string leftImageTopic = "/orig_image_left";
+        const std::string boxTopic = "/bounding_boxes";
 
         ros::NodeHandle nH;
         image_transport::ImageTransport it;
 
         // Image transport subscriber
-        image_transport::Subscriber arImageSub;
+        image_transport::Subscriber leftImageSub;
+        image_transport::Subscriber rightImageSub;
 
         // Publisher
         ros::Publisher objectPub;
 
-        // CV variables
-        bool ok;
-        int hue, sat, val, width;
+        // Window name
+        const std::string kWinName = "Object Detection";
+
+        // Class parameters
+        std::vector<std::string> classes;
+        std::string classesFile = "/home/kylejosling/Downloads/cuke-training/classes.names";
+
+        // Model configuration files
+        cv::String modelConfiguration = "/home/kylejosling/Downloads/cuke-training/darknet-yolov3.cfg";
+        cv::String modelWeights = "/home/kylejosling/Downloads/cuke-training/weights/darknet-yolov3_final.weights";
+
+        // Neural network
+        cv::dnn::Net net;
+
+        // Network parameters
+        int inpWidth = 416;
+        int inpHeight = 416;
 
         cv::Mat frame;
-        cv::Rect2d roi;
-        cv::Ptr<cv::TrackerKCF> tracker;
-
+        cv::Mat blob;
 };
 
 const char* keys =
@@ -70,135 +137,16 @@ int inpWidth = 416;  // Width of network's input image
 int inpHeight = 416; // Height of network's input image
 std::vector<std::string> classes;
 
-// Remove the bounding boxes with low confidence using non-maxima suppression
-void postprocess(cv::Mat& frame, const std::vector<cv::Mat>& out);
 
 // Draw the predicted bounding box
 void drawPred(int classId, float conf, int left, int top, int right, int bottom, cv::Mat& frame);
 
-// Get the names of the output layers
-std::vector<cv::String> getOutputsNames(const cv::dnn::Net& net);
 
 int main(int argc, char** argv) {
 
     std::cout << "CV_major : " << CV_MAJOR_VERSION << std::endl;
     std::cout << "CV_minor : " << CV_MINOR_VERSION << std::endl;
-    cv::CommandLineParser parser(argc, argv, keys);
-    parser.about("Use this script to run object detection using YOLO3 in OpenCV.");
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
-    }
-    // Load names of classes
-    std::string classesFile = "/home/kylejosling/Downloads/test/ObjectDetection-YOLO/coco.names";
-    std::ifstream ifs(classesFile.c_str());
-    std::string line;
-    while (getline(ifs, line)) classes.push_back(line);
 
-    // Give the configuration and weight files for the model
-    cv::String modelConfiguration = "/home/kylejosling/Downloads/test/ObjectDetection-YOLO/yolov3.cfg";
-    cv::String modelWeights = "/home/kylejosling/Downloads/test/ObjectDetection-YOLO/yolov3.weights";
-
-    // Load the network
-    cv::dnn::Net net = cv::dnn::readNetFromDarknet(modelConfiguration, modelWeights);
-
-    // Default backend should be OpenCV
-    net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
-    net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
-
-    // Open a video file or an image file or a camera stream.
-    std::string str, outputFile;
-    cv::VideoCapture cap;
-    cv::VideoWriter video;
-    cv::Mat frame, blob;
-
-    try {
-        
-        outputFile = "yolo_out_cpp.avi";
-        if (parser.has("image"))
-        {
-            // Open the image file
-            str = parser.get<cv::String>("image");
-            std::ifstream ifile(str);
-            if (!ifile) throw("error");
-            cap.open(str);
-            str.replace(str.end()-4, str.end(), "_yolo_out_cpp.jpg");
-            outputFile = str;
-        }
-        else if (parser.has("video"))
-        {
-            // Open the video file
-            str = parser.get<cv::String>("video");
-            std::ifstream ifile(str);
-            if (!ifile) throw("error");
-            cap.open(str);
-            str.replace(str.end()-4, str.end(), "_yolo_out_cpp.avi");
-            outputFile = str;
-        }
-        // Open the webcaom
-        else cap.open(0);
-        
-    }
-    catch(...) {
-        std::cout << "Could not open the input image/video stream" << std::endl;
-        return 0;
-    }
-
-
-    // Get the video writer initialized to save the output video
-    if (!parser.has("image")) {
-        video.open(outputFile, cv::VideoWriter::fourcc('M','J','P','G'), 28, cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)));
-    }
-
-    // Create a window
-    static const std::string kWinName = "Deep learning object detection in OpenCV";
-    namedWindow(kWinName, cv::WINDOW_NORMAL);
-
-    // Process frames.
-    while (cv::waitKey(1) < 0)
-    {
-        // get frame from the video
-        cap >> frame;
-
-        // Stop the program if reached end of video
-        if (frame.empty()) {
-            std::cout << "Done processing !!!" << std::endl;
-            std::cout << "Output file is stored as " << outputFile << std::endl;
-            cv::waitKey(3000);
-            break;
-        }
-
-        // Create a 4D blob from a frame.
-        cv::dnn::blobFromImage(frame, blob, 1/255.0, cvSize(inpWidth, inpHeight), cv::Scalar(0,0,0), true, false);
-
-        //Sets the input to the network
-        net.setInput(blob);
-
-        // Runs the forward pass to get output of the output layers
-        std::vector<cv::Mat> outs;
-        // TODO is this right? old function was causing linker errors 
-        net.forward(outs, getOutputsNames(net));
-
-        // Remove the bounding boxes with low confidence
-        postprocess(frame, outs);
-
-        // Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-        std::vector<double> layersTimes;
-        double freq = cv::getTickFrequency() / 1000;
-        double t = net.getPerfProfile(layersTimes) / freq;
-        std::string label = cv::format("Inference time for a frame : %.2f ms", t);
-        cv::putText(frame, label, cv::Point(0, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255));
-        
-        // Write the frame with the detection boxes
-        cv::Mat detectedFrame;
-        frame.convertTo(detectedFrame, CV_8U);
-        if (parser.has("image")) imwrite(outputFile, detectedFrame);
-        else video.write(detectedFrame);
-        
-        cv::imshow(kWinName, frame);
-
-    }
     // std::string nodeName = "cuke_vision_node";
     // ros::init(argc, argv, nodeName);
 
