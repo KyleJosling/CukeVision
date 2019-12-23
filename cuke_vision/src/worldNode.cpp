edfe410df52beb52936e74a6304a5b75a911ac59
Kyle Josling <kylejosling@hotmail.com>
2019-12-23 16:53:49 -0500

    Added world node and started researching on adding/removing collision objects in MoveIt!

---
+++ cuke_vision/src/worldNode.cpp
@@ -0,0 +1,25 @@
+// ----------------------------------------------------------
+// NAME: World Node
+// DESCRIPTION: Node that handles adding and removing
+// collision objects (cucumbers) from world map
+// ----------------------------------------------------------
+#include "worldNode.hpp"
+
+// Constructor
+worldNode::worldNode() {
+
+}
+
+// Adds a collision object to the world
+void worldNode::addCucumber() {
+
+}
+
+// Removes a collision object from the world
+void worldNode::removeCucumber() {
+
+}
+
+int main (int argc, char** argv ) {
+
+}
